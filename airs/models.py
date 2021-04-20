41b98ed3e2ff9170a247a2d75d67d0b5e5f14d28
shg25 <shg@abeam.ocn.ne.jp>
2021-04-21 02:24:06 +0900

    modelを仮で記述してmakemigration

--- airs/models.py
+++ airs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# 番組
+class Program(models.Model):
+    name = models.CharField(max_length=200) # 番組名
+
+# 放送
+class Air(models.Model):
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    started = models.DateTimeField('date published') # 開始日時
+    ended = models.DateTimeField('date published') # 終了日時
+
+# 何卒（聴取）
+class Nanitozo(models.Model):
+    air = models.ForeignKey(Air, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200) # 感想
