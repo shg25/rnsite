c7436090f2bd6cf3067171179596cceed4f4c472
shg25 <shg@abeam.ocn.ne.jp>
2021-04-27 02:05:06 +0900

    latest_air_listをindexに表示

--- airs/urls.py
+++ airs/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /airs/
     path('', views.index, name='index'),
+    # ex: /airs/5/
+    path('<int:air_id>/', views.detail, name='detail'),
+    # ex: /airs/5/results/
+    path('<int:air_id>/results/', views.results, name='results'),
+    # ex: /airs/5/vote/
+    path('<int:air_id>/vote/', views.vote, name='vote'),
 ]
--- airs/views.py
+++ airs/views.py
@@ -1,4 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Air
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_air_list = Air.objects.order_by('-started')[:5]
+    context = {'latest_air_list': latest_air_list}
+    return render(request, 'airs/index.html', context)
+
+
+def detail(request, air_id):
+    return HttpResponse("You're looking at air %s." % air_id)
+
+
+def results(request, air_id):
+    response = "You're looking at the results of air %s."
+    return HttpResponse(response % air_id)
+
+
+def vote(request, air_id):
+    return HttpResponse("You're voting on air %s." % air_id)
