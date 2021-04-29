c238d06859f44da08bae073188010567cf696857
shg25 <shg@abeam.ocn.ne.jp>
2021-04-30 01:41:56 +0900

    POSTする

--- airs/views.py
+++ airs/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Air
+from .models import Air, Nanitozo
 
 
 def index(request):
@@ -10,9 +10,11 @@ def index(request):
     context = {'latest_air_list': latest_air_list}
     return render(request, 'airs/index.html', context)
 
+
 def detail(request, air_id):
     air = get_object_or_404(Air, pk=air_id)
     return render(request, 'airs/detail.html', {'air': air})
+
 
 def results(request, air_id):
     response = "You're looking at the results of air %s."
@@ -20,4 +22,19 @@ def results(request, air_id):
 
 
 def vote(request, air_id):
-    return HttpResponse("You're voting on air %s." % air_id)
+    air = get_object_or_404(Air, pk=air_id)
+    try:
+        selected_nanitozo = air.nanitozo_set.get(pk=request.POST['nanitozo'])
+    except (KeyError, Nanitozo.DoesNotExist):
+        # Redisplay the air voting form.
+        return render(request, 'airs/detail.html', {
+            'air': air,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_nanitozo.comment += "追加！"
+        selected_nanitozo.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('airs:results', args=(air.id,)))
