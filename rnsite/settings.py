f7e9e49ea6153d55e25112dd823c08a673b46335
shg25 <shg@abeam.ocn.ne.jp>
2021-06-12 14:44:00 +0900

    Heroku向けにファイル追加

--- rnsite/settings.py
+++ rnsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,6 @@ STATIC_URL = '/static/'
 # login、logoutのリダイレクト先をカスタマイズ
 LOGIN_REDIRECT_URL='airs:index'
 LOGOUT_REDIRECT_URL='airs:index'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
