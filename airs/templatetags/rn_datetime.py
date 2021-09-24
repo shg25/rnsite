d9c48067d60afeebdb15fd14bca92cd91623942a
shg25 <shg@abeam.ocn.ne.jp>
2021-09-25 00:50:20 +0900

    regroupで放送と何卒を日付でグループ分けしてリスト表示

--- airs/templatetags/rn_datetime.py
+++ airs/templatetags/rn_datetime.py
@@ -7,29 +7,46 @@ from pytz import timezone as pytztimezone
 register = template.Library()
 
 
+weekday_list = ['月', '火', '水', '木', '金', '土', '日']
+
+
+@register.filter
+def rn_regroup_date(dt):
+    dt = timedelta_midnight_date(dt)
+    return datetime.date(dt.year, dt.month, dt.day)
+
+
+@register.filter
+def rn_weekday(dt):
+    return weekday_list[dt.weekday()]
+
+
+@register.filter
+def rn_md(dt):
+    return str(dt.month) + '/' + str(dt.day)
+
+
 @register.filter
 def init_rn_datetime(started, ended):
     return RNDatetime(started, ended)
 
 
 class RNDatetime:
-    weekday_list = ['月', '火', '水', '木', '金', '土', '日']
-
     def __init__(self, started, ended):
         self.airtime = diff_minutes(started, ended)  # 放送開始日時と放送終了日時の差分から放送時間を取得
 
         started = astimezone_tokyo(started)  # 日本時間にしておく
+        started = timedelta_midnight_date(started)
 
-        # 0〜4時は 日付を[-1日] 時間は[+24h]で表示する（0→24、1→25、2→26、3→27、4→28）
+        # 0〜4時は[+24h]で表示する（0→24、1→25、2→26、3→27、4→28）
         # 終了時刻が5時の場合はラジコ上の終了時刻は29時と表記されるが、当サイトでは終了時刻は表示しないので不要
         if is_midnight(started):
-            started = started + datetime.timedelta(days=-1)  # -1日
             hour = str(started.hour + 24)
         else:
             hour = str(started.hour)
 
-        self.md = str(started.month) + '/' + str(started.day)
-        self.weekday = self.weekday_list[started.weekday()]
+        self.md = rn_md(started)
+        self.weekday = rn_weekday(started)
         self.hm = str(hour) + ':' + str(started.strftime("%M"))
 
 
@@ -39,6 +56,12 @@ def astimezone_tokyo(dt):
 
 def is_midnight(dt):
     return dt.hour < 5
+
+
+def timedelta_midnight_date(dt):
+    if is_midnight(dt):
+        dt = dt + datetime.timedelta(days=-1)  # 0〜4時は日付を[-1日]で表示する
+    return dt
 
 
 def diff_minutes(before, after):
