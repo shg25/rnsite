bbd3c83c9d1eed08242de57f53e43280fe2cbe2e
shg25 <shg@abeam.ocn.ne.jp>
2023-03-23 09:07:32 +0900

    ユーザー詳細画面に月毎の何卒数を表示

--- airs/views/user_views.py
+++ airs/views/user_views.py
@@ -1,5 +1,9 @@
 from django.views import generic
 from django.contrib.auth.models import User
+from django.db.models.functions import TruncMonth, TruncYear
+from django.db.models import Count
+
+from ..models import Nanitozo
 
 
 class UserListView(generic.ListView):
@@ -11,3 +15,26 @@ class UserListView(generic.ListView):
 
 class UserDetailView(generic.DetailView):
     model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # TODO TruncYearとTruncMonthそれぞれで取得してるけど、TruncMonthだけとってYearは計算したほうが良いかも？
+        nanitozo_trunc_year = Nanitozo.objects.filter(user=context['user']).annotate(
+            monthly_date=TruncYear('created_at')
+        ).values('monthly_date').annotate(
+            count=Count('id'),
+        ).values('monthly_date', 'count').order_by('-monthly_date')
+
+        nanitozo_trunc_month = Nanitozo.objects.filter(user=context['user']).annotate(
+            monthly_date=TruncMonth('created_at')
+        ).values('monthly_date').annotate(
+            count=Count('id'),
+        ).values('monthly_date', 'count').order_by('monthly_date')
+
+        context = {
+            'nanitozo_trunc_year': nanitozo_trunc_year,
+            'nanitozo_trunc_month': nanitozo_trunc_month,
+        }
+
+        return context
