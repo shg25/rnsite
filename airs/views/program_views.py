511ff4c123ba7f12a65e5f49bac97582be3b47c8
shg25 <shg@abeam.ocn.ne.jp>
2025-10-02 23:58:03 +0900

    番組一覧を何卒ランキング順に変更

--- airs/views/program_views.py
+++ airs/views/program_views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.views import generic
 
 from ..models import Program
@@ -6,7 +7,12 @@ from ..models import Program
 class ProgramsListView(generic.ListView):
     model = Program
     paginate_by = 40
-    # TODO 放送登録日時降順で表示
+
+    def get_queryset(self):
+        return Program.objects.annotate(
+            air_count=Count('air', distinct=True),
+            nanitozo_count=Count('air__nanitozo', distinct=True)
+        ).filter(nanitozo_count__gt=0).order_by('-nanitozo_count', 'name')
 
 
 class ProgramDetailView(generic.DetailView):
